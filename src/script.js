975c40ef9debb105458d1ff8d653fa666638004c
YitingLiu97 <54900403+YitingLiu97@users.noreply.github.com>
2021-08-27 18:19:39 -0400

    updates with music and more blobs

--- src/script.js
+++ src/script.js
@@ -38,6 +38,40 @@ window.onload = () => {
     entity.setAttribute('gltf-model', model.url);
   };
   
+  // <a-entity rotation="180 90 0" animation="property: rotation; to: 20 -360 -180; loop: true; dur: 50000">
+  // <a-sphere material="shader:displacement-offset" myoffset-updater scale="1 1 1" radius="1" position="0 8 -5"
+  //   segments-height="50" segments-width="50">
+  //   <a-animation attribute="rotation" direction="alternate-reverse" dur="5000" from="1 1 1" to="2 2 2 "
+  //     repeat="indefinite"></a-animation>
+  // </a-sphere>
+
+  // should update scale via three.js 
+
+
+
+
+  const setShader = (shaderObj, entity)=>{
+
+    if (shaderObj.scale) {
+     // entity.setAttribute('scale', shaderObj.scale);
+      entity.object3D.scale.set(1, 2, 3);
+
+      console.log("shader obj scale set to 123 should be "+shaderObj.object3D.scale);
+    }
+  
+    if (shaderObj.rotation) {
+      entity.setAttribute('rotation', shaderObj.rotation);
+    }
+  
+    if (shaderObj.position) {
+      entity.setAttribute('position', shaderObj.position);
+    }
+
+
+entity.setAttribute('material', shaderObj.material);
+
+
+  };
 
   // should place the model in an offset within the range of the device position
   // allow zoom in zoom out?
@@ -54,7 +88,9 @@ window.onload = () => {
       const model = document.createElement('a-entity');
      // model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
       setModel(models[modelIndex], model);
-  
+  const shaderObj = document.getElementById('example');
+      setShader(shaderObj,shaderObj);
+      
       model.setAttribute('animation-mixer', 'repeat');
   
     //   document.querySelector('button[data-action="change"]').addEventListener('click', function () {
